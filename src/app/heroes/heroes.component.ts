70661432349fb4e8abd230c6e48629e9376e7448
Ryze <kzh125@qq.com>
2020-12-28 16:12:25 +0800

    HeroDetailComponent: add routing detail/:id add routerLink: from DashboardComponent and HeroesComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,7 +3,6 @@ import { Observable } from 'rxjs';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 
 @Component({
@@ -13,17 +12,11 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
   heroes$: Observable<Hero[]>;
-  selectedHero: Hero;
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes() {
